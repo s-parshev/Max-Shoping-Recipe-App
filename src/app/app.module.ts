718ca24595210d5e17e9ce5e1079011df081851f
s-parshev <stoyan.parshev@novarto.com>
2022-09-12 15:51:53 +0300

    init setup project. added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopingComponent } from './shoping/shoping.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { ShopingListComponent } from './shoping/shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopingComponent,
+    RecipeComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shoping/shoping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping',
+  templateUrl: './shoping.component.html',
+  styleUrls: ['./shoping.component.css']
+})
+export class ShopingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
