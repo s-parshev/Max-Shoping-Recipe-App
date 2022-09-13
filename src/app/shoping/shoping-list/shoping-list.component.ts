bfff313d889e8f377f91184250e249bbf12b6123
s-parshev <stoyan.parshev@novarto.com>
2022-09-13 11:53:02 +0300

    display ingredient in shoping list. data hardcoded

--- src/app/shoping/shoping-list/shoping-list.component.ts
+++ src/app/shoping/shoping-list/shoping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient }from '../../shared/ingredient.model';
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-  ingredients =[]; 
+  ingredients: Ingredient[] =[
+    new Ingredient("Apples",5),
+    new Ingredient("Potatos",15)
+  ]; 
   constructor() { }
 
   ngOnInit(): void {
