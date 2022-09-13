dfd090f0593dafcaebfe306c0b2fb40be83a0f0d
s-parshev <stoyan.parshev@novarto.com>
2022-09-13 11:44:07 +0300

    shoping-list html temp. ingredient.model create.

--- src/app/shoping/shoping-list/shoping-list.component.ts
+++ src/app/shoping/shoping-list/shoping-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+  ingredients =[]; 
   constructor() { }
 
   ngOnInit(): void {
